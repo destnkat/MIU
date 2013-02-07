8dac05623bd26198f0987f035d6e20275544f206
Daniel Beacham <dkuwf15@gmail.com>
2013-02-06 21:46:52 -0500

    Adding the Images and JSON entries

--- _js/json.js
+++ _js/json.js
@@ -31,6 +31,135 @@ var json = {
             "playlist_priority" : '25',
             "playlist_date" : '2013-02-20',
             "enabled" : '0'
+        },
+        {
+            "playlist_name" : 'Playlist 5',
+            "playlist_genre" : 'country',
+            "playlist_description" : 'Another Country Playist',
+            "playlist_priority" : '25',
+            "playlist_date" : '2013-02-20',
+            "enabled" : '1'
+        },
+        {
+            "playlist_name" : 'Playlist 6',
+            "playlist_genre" : 'dubstep',
+            "playlist_description" : 'Dubstepping through the ages',
+            "playlist_priority" : '25',
+            "playlist_date" : '2013-02-20',
+            "enabled" : '0'
+        },
+        {
+            "playlist_name" : 'Playlist 7',
+            "playlist_genre" : 'rap',
+            "playlist_description" : 'I can beatbox and so can you',
+            "playlist_priority" : '55',
+            "playlist_date" : '2013-02-20',
+            "enabled" : '0'
+        },
+        {
+            "playlist_name" : 'Playlist 8',
+            "playlist_genre" : 'country',
+            "playlist_description" : 'Dogs ran over and drunken nights',
+            "playlist_priority" : '40',
+            "playlist_date" : '2013-02-20',
+            "enabled" : '0'
+        },
+        {
+            "playlist_name" : 'Playlist 9',
+            "playlist_genre" : 'country',
+            "playlist_description" : 'Pretty Sure my wife left me',
+            "playlist_priority" : '33',
+            "playlist_date" : '2013-02-20',
+            "enabled" : '1'
+        },
+        {
+            "playlist_name" : 'Playlist 10',
+            "playlist_genre" : 'rap',
+            "playlist_description" : 'My pants are saggin to the ground baby',
+            "playlist_priority" : '12',
+            "playlist_date" : '2013-02-20',
+            "enabled" : '1'
+        },
+        {
+            "playlist_name" : 'Playlist 11',
+            "playlist_genre" : 'pop',
+            "playlist_description" : 'We are all dancers and guys who wear earrings',
+            "playlist_priority" : '78',
+            "playlist_date" : '2013-02-20',
+            "enabled" : '0'
+        },
+        {
+            "playlist_name" : 'Playlist 12',
+            "playlist_genre" : 'pop',
+            "playlist_description" : 'Backstreet Boys got famous and here is how',
+            "playlist_priority" : '25',
+            "playlist_date" : '2013-02-20',
+            "enabled" : '0'
+        },
+        {
+            "playlist_name" : 'Playlist 13',
+            "playlist_genre" : 'pop',
+            "playlist_description" : 'I love Britney Spears',
+            "playlist_priority" : '99',
+            "playlist_date" : '2013-02-20',
+            "enabled" : '1'
+        },
+        {
+            "playlist_name" : 'Playlist 14',
+            "playlist_genre" : 'pop',
+            "playlist_description" : 'Boy Bands, Britney and Christina.. boom',
+            "playlist_priority" : '95',
+            "playlist_date" : '2013-02-20',
+            "enabled" : '1'
+        },
+        {
+            "playlist_name" : 'Playlist 15',
+            "playlist_genre" : 'dubstep',
+            "playlist_description" : 'I think Im in the matrix',
+            "playlist_priority" : '25',
+            "playlist_date" : '2013-02-20',
+            "enabled" : '1'
+        },
+        {
+            "playlist_name" : 'Playlist 16',
+            "playlist_genre" : 'rap',
+            "playlist_description" : 'Eminem is great',
+            "playlist_priority" : '80',
+            "playlist_date" : '2013-02-20',
+            "enabled" : '0'
+        },
+        {
+            "playlist_name" : 'Playlist 17',
+            "playlist_genre" : 'country',
+            "playlist_description" : 'My pickup truck is awesome!!',
+            "playlist_priority" : '20',
+            "playlist_date" : '2013-02-20',
+            "enabled" : '0'
+        },
+        {
+            "playlist_name" : 'Playlist 18',
+            "playlist_genre" : 'dubstep',
+            "playlist_description" : 'Another Dubstep production',
+            "playlist_priority" : '12',
+            "playlist_date" : '2013-02-20',
+            "enabled" : '0'
+        },
+        {
+            "playlist_name" : 'Playlist 19',
+            "playlist_genre" : 'country',
+            "playlist_description" : 'Yeeehaw',
+            "playlist_priority" : '65',
+            "playlist_date" : '2013-02-20',
+            "enabled" : '0'
+        },
+        {
+            "playlist_name" : 'Playlist 20',
+            "playlist_genre" : 'pop',
+            "playlist_description" : 'I ran out of things to say',
+            "playlist_priority" : '80',
+            "playlist_date" : '2013-02-20',
+            "enabled" : '0'
         }
+
     ]
 }
