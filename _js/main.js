e965cb5b605168e16d448d19ddba455f6db2d6dc
Daniel Beacham <dbeacham@contractor.ea.com>
2013-02-07 17:56:59 -0500

    Refactoring the Codebase to use JQM and jQuery

--- _js/main.js
+++ _js/main.js
@@ -1,101 +1,98 @@
+/**
+ * @todo CLEAN THIS CODE UP as I learn more about JQM. Convert all items to use jQuery and not native JS.
+ * @author Daniel C. Beacham
+ * @date 02.07.13
+ * @description - Using JQM to convert VFW Web App into a Mobile Version.  Using Multi-Page Templating
+ */
+$('#home').live('pageshow', function(event){
+    $('#list_browse').find('li').each(function(){
+       $(this).find('a').click(function(){
+           localStorage.setItem('criteria', $(this).data('criteria'));
+       });
+    });
+});
+
+$('#browse').live('pageshow', function(event){
+    if (localStorage.length < 2 ){
+        retrieveData();
+    } else {
+        displaySelectedData();
+    }
+});
+
+$('#addItem').live('pageshow', function(event){
+
+});
+
 function getRandomPlaylistId() {
     return Math.floor(Math.random() * (1000 - 1 + 1)) + 1;
 }
 
-function populateLocalStorage() {
-    var dummyDataLoaded = false;
+function retrieveData() {
+    var storageObj = {};
+    storageObj.playlists = [];
 
-    if(localStorage.length == 0 ){
-        var dummyData = json.playlists;
-
-        for (var i = 0; i < dummyData.length; i++) {
-            var playlistID = getRandomPlaylistId();
-            var consolidated = JSON.stringify(dummyData[i]);
-            localStorage.setItem(playlistID, consolidated);
-        }
-
-        dummyDataLoaded = true;
-    }
-
-    displayTheData();
-}
-
-function getDisplayCriteria(param) {
-    var url = window.location.search.substring(1);
-    var parameters = url.split('&');
-    for (var i = 0; i < parameters.length; i++)
-    {
-        var parameterKey = parameters[i].split('=');
-        if (parameterKey[0] == param)
-        {
-            return parameterKey[1];
-        }
-    }
-
-    return false;
-}
-
-function passesFilter(criteria, json) {
-    if ((criteria === 'country' && json.playlist_genre.toLowerCase() === 'country') ||
-        (criteria === 'rap' && json.playlist_genre.toLowerCase()  === 'rap') ||
-        (criteria === 'dubstep' && json.playlist_genre.toLowerCase()  === 'dubstep') ||
-        (criteria === 'pop' && json.playlist_genre.toLowerCase()  === 'pop') ||
-        (criteria === 'inactive' && json.enabled.toLowerCase()  === '0') ||
-        (criteria === 'high_priority' && json.playlist_priority > 75)
-        ) {
-        return true;
-    }
-
-    return false;
-}
-
-function displayTheData() {
-    var criteria           = getDisplayCriteria('criteria');
-    var container          = document.getElementById('browse_data');
-    var categoryContainer  = document.getElementById('category_label_id');
-    var output             = "";
-
-    if (criteria) {
-        for(var i = 0; i < localStorage.length; i++) {
-            var key = localStorage.key(i);
-            var value = JSON.parse(localStorage[key]);
-
-            if(passesFilter(criteria, value)) {
-                var enabled = value.enabled == "1" ? "Active" : "Inactive";
-                output += "<div class='display_item'><div class='genreIcon " + value.playlist_genre +"'>" + value.playlist_genre + " ICON</div>";
-                output += "<p><strong>Name: </strong> " +value.playlist_name +  "</p>";
-                output += "<p><strong>Description: </strong>" + value.playlist_description + "</p>";
-                output += "<p><strong>Genre: </strong>" + value.playlist_genre + "</p>";
-                output += "<p><strong>Created: </strong>" + value.playlist_date + "</p>";
-                output += "<p><strong>Priority: </strong>" + value.playlist_priority + "</p>";
-                output += "<p><strong>Status: </strong>" + enabled + "</p>";
-                output += "<div class='playlist_control_buttons'>";
-                output += "<input type='button' value='edit' onclick='editLocalStorageItem(this.id);' id='edit_" + key + "' />";
-                output += "<input type='button' value='delete' onclick='deleteLocalStorageItem(this.id);' id='delete_" + key + "' /></div></div>";
+    $.ajax({
+        url: '_js/json.json',
+        success: function(data) {
+            for(var i = 0; i < data.playlists.length; i++) {
+                var playlistID = getRandomPlaylistId();
+                var consolidated = JSON.stringify(data.playlists[i]);
+                localStorage.setItem(playlistID, consolidated);
             }
+
+            displaySelectedData();
+        }
+    });
+}
+
+function displaySelectedData() {
+    var criteria = localStorage.getItem('criteria');
+    var output = "";
+    for (var i= 0; i < localStorage.length; i++) {
+        var key = localStorage.key(i);
+
+        //Disregard the Criteria LS Item
+        if(key === 'criteria') {
+            continue;
         }
 
-       categoryContainer.innerHTML = criteria;
-       container.innerHTML = output;
+        var tmpItem = $.parseJSON(localStorage[key]);
+
+        if ((criteria === 'country' && tmpItem.playlist_genre.toLowerCase() === 'country') ||
+            (criteria === 'rap' && tmpItem.playlist_genre.toLowerCase()  === 'rap') ||
+            (criteria === 'dubstep' && tmpItem.playlist_genre.toLowerCase()  === 'dubstep') ||
+            (criteria === 'pop' && tmpItem.playlist_genre.toLowerCase()  === 'pop') ||
+            (criteria === 'inactive' && tmpItem.enabled.toLowerCase()  === '0') ||
+            (criteria === 'high_priority' && tmpItem.playlist_priority > 75) ||
+            (criteria === 'all')
+            ) {
+            var enabled = tmpItem.enabled == "1" ? "Active" : "Inactive";
+            output += "<div class='display_item'>";
+            output += "<div class='genreIcon " + tmpItem.playlist_genre +"'>" + tmpItem.playlist_genre + " ICON</div>";
+            output += "<p><strong>Name: </strong> " + tmpItem.playlist_name +  "</p>";
+            output += "<p><strong>Description: </strong>" + tmpItem.playlist_description + "</p>";
+            output += "<p><strong>Genre: </strong>" + tmpItem.playlist_genre + "</p>";
+            output += "<p><strong>Created: </strong>" + tmpItem.playlist_date + "</p>";
+            output += "<p><strong>Priority: </strong>" + tmpItem.playlist_priority + "</p>";
+            output += "<p><strong>Status: </strong>" + enabled + "</p>";
+            output += "<input type='button' value='edit playlist' onclick='editLocalStorageItem(this.id);' id='edit_" + key + "' />";
+            output += "<input type='button' value='delete playlist' onclick='deleteLocalStorageItem(this.id);' id='delete_" + key + "' />";
+            output += '</div>';
+        }
     }
+
+    $('#browse_results').html(output);
 }
 
-function bindAllButtons() {
-    var btnAdd = document.getElementById('btn_addPlaylist');
-
-    if(btnAdd) {
-        btnAdd.addEventListener('touchstart', test, false);
-    }
+function getRandomPlaylistId() {
+    return Math.floor(Math.random() * (1000 - 1 + 1)) + 1;
 }
 
-function test(e) {
-    console.log('test');
-}
+function deleteLocalStorageItem(key) {
+    var delim = key.indexOf('_');
+    var cleanId = key.substring(delim + 1, key.length);
+    localStorage.removeItem(cleanId);
 
-function init(pg) {
-    bindAllButtons();
-
-    if(pg === 'display') {
-        populateLocalStorage();
-    }
+    displaySelectedData();
 }
