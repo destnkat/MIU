66cabd02353e5f69cadd527a58f805dcf74c2a11
Daniel Beacham <dkuwf15@gmail.com>
2013-02-07 00:37:11 -0500

    More updates

--- _js/main.js
+++ _js/main.js
@@ -1,263 +1,101 @@
+function getRandomPlaylistId() {
+    return Math.floor(Math.random() * (1000 - 1 + 1)) + 1;
+}
 
-    function saveFormData() {
+function populateLocalStorage() {
+    var dummyDataLoaded = false;
 
-        var savedItems = {},
-            itemsArray = ['playlist_name', 'playlist_description', 'playlist_genre', 'playlist_date', 'playlist_priority'],
-            playlistId = getRandomPlaylistId(),
-            consolidated,
-            addEdit = document.getElementById('add_edit').value,
-            confirmationMsg = '<p>Your Playlist has been successfully saved!</p>';
+    if(localStorage.length == 0 ){
+        var dummyData = json.playlists;
 
-        for(var i = 0; i < itemsArray.length; i++ ) {
-            var key = itemsArray[i];
-            savedItems[key] = document.getElementById(key).value;
+        for (var i = 0; i < dummyData.length; i++) {
+            var playlistID = getRandomPlaylistId();
+            var consolidated = JSON.stringify(dummyData[i]);
+            localStorage.setItem(playlistID, consolidated);
         }
 
-
-        if (addEdit.indexOf('edit') !== -1) {
-            var tmp = addEdit.indexOf('_');
-            playlistId = addEdit.substr(tmp + 1, addEdit.length);
-            confirmationMsg = '<p>Your Playlist has been edited successfully!</p>'
-        }
-
-        savedItems['enabled'] = checkEnabledStatus();
-        consolidated = JSON.stringify(savedItems);
-
-
-        localStorage.setItem(playlistId, consolidated);
-        var conf = document.getElementById('confirmation_error');
-        conf.style.display = 'block';
-        conf.innerHTML = confirmationMsg;
-
-        return false;  // Don't submit the Form through the PHP
+        dummyDataLoaded = true;
     }
 
-    function validateTheForm(frm) {
-        var error = false;
-        var conf = document.getElementById('confirmation_error');
-        var arr = ['lbl_name', 'lbl_description', 'lbl_genre'];
-        var errors = [];
-        conf.innerHTML = '';
+    displayTheData();
+}
 
-
-        for (var i= 0; i < arr.length; i++) {
-            var tmplbl = document.getElementById(arr[i]);
-            removeClass(tmplbl, 'hasError');
-        }
-
-        conf.style.display = 'block';
-
-        //Clear the Error DIV
-        conf.innerHTML = '';
-
-        if(frm.playlist_name.value == '') {
-            error = true;
-            errors.push('lbl_name');
-        }
-
-        if (frm.playlist_description.value == '') {
-            error = true;
-            errors.push('lbl_description');
-        }
-
-        if(frm.playlist_genre.value === 'choose one') {
-            error = true;
-            errors.push('lbl_genre');
-        }
-
-        if (error) {
-            conf.innerHTML = '<p>There are issues with your submission. Please see the highlighted fields and' +
-                ' make the necessary updates</p>';
-
-            conf.style.display = 'block';
-
-            for(var i=0; i < errors.length; i++) {
-                var tmpLbl = document.getElementById(errors[i]);
-                addClass(tmpLbl, 'hasError');
-            }
-
-
-        } else {
-            saveFormData();
-        }
-
-        return false;
-
-
-    }
-
-    function checkEnabledStatus() {
-        if(document.getElementById('playlist_enabled').checked) {
-            return '1';
-        } else {
-            return '0';
+function getDisplayCriteria(param) {
+    var url = window.location.search.substring(1);
+    var parameters = url.split('&');
+    for (var i = 0; i < parameters.length; i++)
+    {
+        var parameterKey = parameters[i].split('=');
+        if (parameterKey[0] == param)
+        {
+            return parameterKey[1];
         }
     }
 
-    function clearFormData(evt) {
-        localStorage.clear();
+    return false;
+}
+
+function passesFilter(criteria, json) {
+    if ((criteria === 'country' && json.playlist_genre.toLowerCase() === 'country') ||
+        (criteria === 'rap' && json.playlist_genre.toLowerCase()  === 'rap') ||
+        (criteria === 'dubstep' && json.playlist_genre.toLowerCase()  === 'dubstep') ||
+        (criteria === 'pop' && json.playlist_genre.toLowerCase()  === 'pop') ||
+        (criteria === 'inactive' && json.enabled.toLowerCase()  === '0') ||
+        (criteria === 'high_priority' && json.playlist_priority > 75)
+        ) {
+        return true;
     }
 
-    function displayFormData() {
+    return false;
+}
 
-        var items = [];
-        var addWrapper = document.getElementById('add_playlist');
-        var displayWrapper = document.getElementById('display_playlists');
-        var existingData = document.getElementById('existing_data');
-        var conf = document.getElementById('confirmation_error');
-        var output = '';
-        var dummyDataLoaded = false;
+function displayTheData() {
+    var criteria           = getDisplayCriteria('criteria');
+    var container          = document.getElementById('browse_data');
+    var categoryContainer  = document.getElementById('category_label_id');
+    var output             = "";
 
-        conf.innerHTML = '';
-        conf.style.display = 'none';
-        addWrapper.style.display = "none";
-        displayWrapper.style.display = "block";
-
-
-        if(localStorage.length == 0 ){
-            var dummyData = json.playlists;
-            for(var i = 0; i < dummyData.length; i++) {
-                var playlistID = getRandomPlaylistId();
-                var consolidated = JSON.stringify(dummyData[i]);
-                localStorage.setItem(playlistID, consolidated);
-            }
-            dummyDataLoaded = true;
-        }
-
+    if (criteria) {
         for(var i = 0; i < localStorage.length; i++) {
             var key = localStorage.key(i);
             var value = JSON.parse(localStorage[key]);
 
-            var enabled = value.enabled == "1" ? "Active" : "Inactive";
-            output += "<div class='genreIcon " + value.playlist_genre +"'>" + value.playlist_genre + " ICON</div>";
-            output += "<p><strong>Name: </strong> " +value.playlist_name +  "</p>";
-            output += "<p><strong>Description: </strong>" + value.playlist_description + "</p>";
-            output += "<p><strong>Genre: </strong>" + value.playlist_genre + "</p>";
-            output += "<p><strong>Created: </strong>" + value.playlist_date + "</p>";
-            output += "<p><strong>Priority: </strong>" + value.playlist_priority + "</p>";
-            output += "<p><strong>Status: </strong>" + enabled + "</p>";
-            output += "<input type='button' value='edit playlist' onclick='editLocalStorageItem(this.id);' id='edit_" + key + "' />";
-            output += "<input type='button' value='delete playlist' onclick='deleteLocalStorageItem(this.id);' id='delete_" + key + "' />  <hr/>";
-
-
-        }
-
-        existingData.innerHTML = output;
-
-        if(dummyDataLoaded) {
-            alert('Dummy Data Loaded');
-        }
-
-    }
-
-    function populateSelectForm() {
-        var genres = ["choose one","country", "rap", "dubstep", "pop"];
-        var genreSelect = document.getElementById('playlist_genre');
-
-        if(genreSelect) {
-
-            for(var i = 0; i < genres.length; i++) {
-                var opt = document.createElement('option');
-                opt.setAttribute('value', genres[i]);
-                opt.innerHTML = genres[i];
-                genreSelect.appendChild(opt);
+            if(passesFilter(criteria, value)) {
+                var enabled = value.enabled == "1" ? "Active" : "Inactive";
+                output += "<div class='display_item'><div class='genreIcon " + value.playlist_genre +"'>" + value.playlist_genre + " ICON</div>";
+                output += "<p><strong>Name: </strong> " +value.playlist_name +  "</p>";
+                output += "<p><strong>Description: </strong>" + value.playlist_description + "</p>";
+                output += "<p><strong>Genre: </strong>" + value.playlist_genre + "</p>";
+                output += "<p><strong>Created: </strong>" + value.playlist_date + "</p>";
+                output += "<p><strong>Priority: </strong>" + value.playlist_priority + "</p>";
+                output += "<p><strong>Status: </strong>" + enabled + "</p>";
+                output += "<div class='playlist_control_buttons'>";
+                output += "<input type='button' value='edit' onclick='editLocalStorageItem(this.id);' id='edit_" + key + "' />";
+                output += "<input type='button' value='delete' onclick='deleteLocalStorageItem(this.id);' id='delete_" + key + "' /></div></div>";
             }
         }
+
+       categoryContainer.innerHTML = criteria;
+       container.innerHTML = output;
     }
-
-    function getRandomPlaylistId() {
-        return Math.floor(Math.random() * (1000 - 1 + 1)) + 1;
-    }
-
-    function bindButtons() {
-        var clear = document.getElementById('clear_data');
-        if (clear)
-            clear.addEventListener('click', clearFormData);
-
-        var display = document.getElementById('display_data');
-        if (display)
-            display.addEventListener('click', displayFormData);
-
-        var addNew = document.getElementById('add_new_playlist');
-        if (addNew) {
-            addNew.addEventListener('click', function(){
-               window.location.href = "addItem.html";
-            });
-        }
-
-        var home = document.getElementById('home_button');
-        if (home) {
-            home.addEventListener('click', function(){
-                window.location.href = 'index.html';
-            });
-        }
-    }
-
-    function hasClass(el, name) {
-        return new RegExp('(\\s|^)'+name+'(\\s|$)').test(el.className);
-    }
-
-    function addClass(el, name)
-    {
-        if (!hasClass(el, name)) { el.className += (el.className ? ' ' : '') +name; }
-    }
-
-    function removeClass(el, name)
-    {
-        if (hasClass(el, name)) {
-            el.className=el.className.replace(new RegExp('(\\s|^)'+name+'(\\s|$)'),' ').replace(/^\s+|\s+$/g, '');
-        }
-    }
-
-    function deleteLocalStorageItem(id) {
-        var delim = id.indexOf('_');
-        var cleanId = id.substring(delim + 1, id.length);
-        localStorage.removeItem(cleanId);
-
-        displayFormData();
-    }
-
-    function editLocalStorageItem(id) {
-        var delim = id.indexOf('_');
-        var cleanId = id.substring(delim + 1, id.length);
-        var retrieveItem = JSON.parse(localStorage.getItem(cleanId));
-
-        var addWrapper = document.getElementById('add_playlist');
-        var displayWrapper = document.getElementById('display_playlists');
-
-        addWrapper.style.display = "block";
-        displayWrapper.style.display = "none";
-        var name = document.getElementById('playlist_name');
-        var description = document.getElementById('playlist_description');
-        var genre = document.getElementById('playlist_genre');
-        var date = document.getElementById('playlist_date');
-        var priority = document.getElementById('playlist_priority');
-        var addEdit = document.getElementById('add_edit');
-
-        name.value = retrieveItem.playlist_name;
-        description.value = retrieveItem.playlist_description;
-        date.value = retrieveItem.playlist_date;
-        priority.value = retrieveItem.playlist_priority;
-
-        if(retrieveItem.enabled == '1') {
-            document.getElementById('playlist_enabled').checked = true;
-        } else {
-            document.getElementById('playlist_disabled').checked = true;
-        }
-
-        for(var i=0; i < genre.length; i++) {
-            if(genre[i].value === retrieveItem.playlist_genre) {
-                genre[i].selected = true;
-            }
-        }
-        document.getElementById('btn_submit').value = 'Edit Playlist';
-        addEdit.value = 'edit_' + cleanId;
-
-    }
-
-function init() {
-    populateSelectForm();
-    bindButtons();
 }
 
-window.onload = init;
+function bindAllButtons() {
+    var btnAdd = document.getElementById('btn_addPlaylist');
+
+    if(btnAdd) {
+        btnAdd.addEventListener('touchstart', test, false);
+    }
+}
+
+function test(e) {
+    console.log('test');
+}
+
+function init(pg) {
+    bindAllButtons();
+
+    if(pg === 'display') {
+        populateLocalStorage();
+    }
+}
